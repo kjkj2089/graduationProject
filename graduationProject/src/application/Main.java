de190ce810bb06009e7e1ef813b73e8b50b94e67
kjkj2089 <kjkj2089@gmail.com>
2022-12-23 18:08:08 +0900

    졸업작품

---
+++ graduationProject/src/application/Main.java
@@ -0,0 +1,37 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Video Search");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+
+	}
+	
+
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
